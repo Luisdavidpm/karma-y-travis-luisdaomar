f43b055ec6af7f498ace15c3339f4d3d7d85f818
alu0100819182 <alu0100819182@ull.edu.es>
2016-03-31 16:10:38 +0100

    De nuevo

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
       'js/main.js',
       'js/medida.js',
       'js/temperatura.js',
-      'js/xregexp.js'
+      'js/xregexp.js',
       'index.html'
     ],
 
